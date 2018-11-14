bd7758774692fe421ac2c0745690fa3bd383c13e
Mimiza <dev@mimiza.com>
2018-11-14 17:17:08 +0700

    next update

--- loader.js
+++ loader.js
@@ -7,25 +7,15 @@ class Ajax {
     }
 }
 
-const loadBot = file => {
-    let url =
-        "https://raw.githubusercontent.com/mmzroot/nanobots/master/nanobots/" +
-        file
+const base = "https://raw.githubusercontent.com/mmzroot/nanobots/master"
+
+const load = file => {
+    let url = base + "/nanobots/" + file
     new Ajax(url, res => {
         if (res.target.responseText) eval(res.target.responseText)
     })
 }
 
-const nanobotsUrl =
-    "https://api.github.com/repos/mmzroot/nanobots/contents/nanobots/"
+const bots = ["hello-world.js", "hi-clients.js"]
 
-//get list of all nanobots
-let nanobotsList = new Ajax(nanobotsUrl, res => {
-    if (res.target.responseText) {
-        res = JSON.parse(res.target.responseText)
-        res.forEach(element => {
-            console.log(element.name)
-            new LoadBot(element.name)
-        })
-    }
-})
+bots.map(e => load(e))
