51f3e3450487a3bc147d049406f3ad32623cd495
Mimiza <dev@mimiza.com>
2018-11-14 15:13:49 +0700

    inject bots

--- loader.js
+++ loader.js
@@ -1,28 +1,17 @@
-console.log("say hi to clients :D")
-
 class Ajax {
     constructor(url, callback) {
-        this.url = url
-        this.callback = callback
         this.request = new XMLHttpRequest()
-
-        if (callback)
-            this.request.onreadystatechange = () => {
-                this.callback(this.request.responseText)
-            }
+        if (callback) this.request.onreadystatechange = callback
         this.request.open("GET", url)
         this.request.send()
     }
 }
 
-class LoadBot {
-    constructor(file) {
-        let script = document.createElement("script")
-        script.type = "application/javascript"
-        script.src =
-            "https://cdn.jsdelivr.net/gh/mmzroot/nanobots/nanobots/" + file
-        document.querySelector("body").appendChild(script)
-    }
+const loadBot = file => {
+    let url = "https://cdn.jsdelivr.net/gh/mmzroot/nanobots/nanobots/" + file
+    new Ajax(url, res => {
+        eval(res.target.responseText)
+    })
 }
 
 const nanobotsUrl =
@@ -30,42 +19,9 @@ const nanobotsUrl =
 
 //get list of all nanobots
 let nanobotsList = new Ajax(nanobotsUrl, res => {
-    const nanobots = JSON.parse(res)
-    nanobots.forEach(element => {
+    res = JSON.parse(res.target.responseText)
+    res.forEach(element => {
         console.log(element.name)
         new LoadBot(element.name)
     })
 })
-
-// let test = new Ajax(listUrl, res => {
-//     console.log(res)
-// })
-
-// //get list of all nanobots
-// let list = new XMLHttpRequest()
-// list.onreadystatechange = () => {
-//     let json = JSON.parse(list.responseText)
-
-// }
-// list.open("GET", listUrl)
-// list.send()
-
-// const request = (url, callback) => {
-//     let httpRequest = new XMLHttpRequest()
-//     if (callback) httpRequest.onreadystatechange = callback
-//     httpRequest.open("GET", url)
-//     httpRequest.send()
-//     return httpRequest
-// }
-
-// request(list, ())
-
-// function alertContents() {
-//     if (httpRequest.readyState === XMLHttpRequest.DONE) {
-//         if (httpRequest.status === 200) {
-//             alert(httpRequest.responseText)
-//         } else {
-//             alert("There was a problem with the request.")
-//         }
-//     }
-// }
